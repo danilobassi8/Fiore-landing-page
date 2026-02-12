8cedc02845c95f241a48222b83f39bdc1d3987a0
Danilo Bassi <danilo-bassi@hotmail.com>
2026-02-12 12:32:14 -0300

    add gmap direction

--- configs/constants.ts
+++ configs/constants.ts
@@ -5,10 +5,10 @@ export const CONSTANTS = {
     url: 'https://wa.me/5493468411130',
   },
   location: {
-    text: 'Consultorio Rosario, SF',
-    url: 'https://maps.google.com/?q=Consultorio+Psicológico+Rosario+Santa+Fe',
+    text: 'Alberdi 544, Corral de Bustos, Córdoba',
+    url: 'https://www.google.com/maps/place/Alberdi+544,+X2645+Corral+de+Bustos-Ifflinger,+C%C3%B3rdoba/@-33.2874887,-62.1900756,17z/data=!4m9!1m2!2m1!1sAlberdi+544!3m5!1s0x95c90240700bda91:0xca442182156ce2ac!8m2!3d-33.2874888!4d-62.1928597!15sCgtBbGJlcmRpIDU0NJIBEGdlb2NvZGVkX2FkZHJlc3PgAQA?entry=ttu&g_ep=EgoyMDI2MDIwOS4wIKXMDSoASAFQAw%3D%3D',
   },
   linkedin: {
-    url: 'https://www.linkedin.com/in/fiorella-pedr%C3%B3s-b227331b7/'
-  }
+    url: 'https://www.linkedin.com/in/fiorella-pedr%C3%B3s-b227331b7/',
+  },
 };
